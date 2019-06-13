fa22fc9a68d7b199424305d859ea1c8a286dc08a
Steven Curtis <stevenpcurtis@hotmail.com>
2019-06-13 20:46:42 +0800

    Added save and delete core data

--- CoreDataToDo/Constants.swift
+++ CoreDataToDo/Constants.swift
@@ -7,9 +7,17 @@
 //
 
 import Foundation
+import UIKit
 
 struct Constants {
     static let entityName = "TaskEntity"
     static let entityNameAttribute = "task"
     static let entityCompletedattribute = "completed"
+    static let alertStoryBoard = "Alerts"
+    
+    struct alerts {
+        static let mainAlert = "AlertViewController"
+        static let grayColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        static let bgColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1.0)
+    }
 }
--- CoreDataToDo/ViewController.swift
+++ CoreDataToDo/ViewController.swift
@@ -14,26 +14,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var tasks = [NSManagedObject]()
+    var objectContext: NSManagedObjectContext! = nil
+    var entity: NSEntityDescription! = nil
+    
+    // alert must be within the view to keep it in scope, when added to the Window
+    var alert: AlertViewController?
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        alert = UIStoryboard(name: Constants.alertStoryBoard, bundle: nil).instantiateViewController(withIdentifier: Constants.alerts.mainAlert) as? AlertViewController
+        alert?.presentToWindow()
+        alert?.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: Constants.entityName)
+        do {
+            tasks = try objectContext.fetch(fetchRequest)
+            if tasks.count == 0 {
+                let entityOne = NSEntityDescription.insertNewObject(forEntityName: Constants.entityName, into: objectContext)
+                entityOne.setValue(false, forKey: Constants.entityCompletedattribute)
+                entityOne.setValue("Enter your task", forKey: Constants.entityNameAttribute)
+                tasks.append(entityOne)
+            }
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
-        
-        let objectContext = appDelegate.persistentContainer.viewContext
-        let entityOne = NSEntityDescription.insertNewObject(forEntityName: Constants.entityName, into: objectContext)
-        entityOne.setValue(false, forKey: Constants.entityCompletedattribute)
-        entityOne.setValue("Enter your task", forKey: Constants.entityNameAttribute)
-
-        tasks.append(entityOne)
-        
-        print (tasks)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        objectContext = appDelegate.persistentContainer.viewContext
+        entity = NSEntityDescription.entity(forEntityName: Constants.entityName, in: objectContext)!
     }
-
-
+    
+    func save(task: String) {
+        let taskObject = NSManagedObject(entity: entity, insertInto: objectContext)
+        taskObject.setValue(task, forKeyPath: Constants.entityNameAttribute)
+        
+        do {
+            try objectContext.save()
+            tasks.append(taskObject)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
 }
 
 extension ViewController: UITableViewDataSource {
@@ -46,6 +74,26 @@ extension ViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = tasks[indexPath.row].value(forKeyPath: Constants.entityNameAttribute) as? String
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            let task = tasks[indexPath.row]
+            tasks.remove(at: indexPath.row)
+            objectContext.delete(task)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+}
+
+extension ViewController: AlertsDelegate {
+    func textValue(textFieldValue: String) {
+        save(task: textFieldValue)
+        tableView.reloadData()
     }
     
     
